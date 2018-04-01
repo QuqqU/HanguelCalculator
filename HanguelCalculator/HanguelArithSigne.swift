2abaf44d2670dfec34315bcc16b12624b35f309c
QuqqU <rldnd913@naver.com>
2018-04-02 00:28:57 +0900

    add translating

--- HanguelCalculator/HanguelArithSigne.swift
+++ HanguelCalculator/HanguelArithSigne.swift
@@ -13,5 +13,25 @@ import Foundation
 class HanguelArithSigne {
     static let shared = HanguelArithSigne()
     
- 
+    static func arithSigneToHanguel(Symbol s: String) -> String? {
+        switch s {
+        case "+" : return "더하기"
+        case "-" : return "빼기"
+        case "*" : return "곱하기"
+        case "/" : return "나누기"
+        default : return nil
+        }
+    }
+
+    static func arithSigneToHanguel(Number n: Int) -> String? {
+        var symbol: String
+        switch n {
+        case 0 : symbol = "+"
+        case 1 : symbol = "-"
+        case 2 : symbol = "*"
+        case 3 : symbol = "/"
+        default: return nil
+        }
+        return arithSigneToHanguel(Symbol: symbol)
+    }
 }
--- HanguelCalculator/ViewController.swift
+++ HanguelCalculator/ViewController.swift
@@ -10,29 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var problemAnswer: Int?
+    var cnt: Int = 0
+    var timer = Timer()
+    
+    @IBOutlet weak var remainedTime: UILabel!
     @IBOutlet weak var numberOfCorrect: UILabel!
     @IBOutlet weak var problem: UILabel!
     @IBOutlet weak var answer: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        self.setTimer()
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(1),
+                                         target: self,
+                                         selector: #selector(setRemainTime),
+                                         userInfo: nil,
+                                         repeats: true)
+    }
+    
+    func setTimer() {
+        makeProblem()
+        remainedTime.text = "10"
+        cnt = 10
+    }
+    
+    @objc func setRemainTime() {
+        if cnt < 0 {
+            timer.invalidate()
+            return
+        }
+        remainedTime.text = "\(cnt)"
+        cnt -= 1
     }
     
     func makeProblem() {
-        let a = arc4random() % 100
-        let b = arc4random() % 100
+        var numA = Int(arc4random() % 100)
+        var numB = Int(arc4random() % 100)
+        let a = HanguelNumber.intToHanguel(Number: numA)
+        let b = HanguelNumber.intToHanguel(Number: numB)
+        let symbol = HanguelArithSigne.arithSigneToHanguel(Number: Int(arc4random() % 4))
+        if symbol == "빼기" && numA < numB { (numA, numB) = (numB, numA) }
         
-        
-        
+        if let _a = a, let _b = b, let _symbol = symbol {
+            problem.text = _a + _symbol + _b
+            problemAnswer = numA + numB //
+        }
     }
 
-
+    func checkAnswer(Answer ans: String?) -> Bool {
+        if Int(ans!) == problemAnswer { return true }
+        else { return false }
+    }
 
     @IBAction func summitAnswer(_ sender: Any) {
-        
-        
+        if checkAnswer(Answer: answer.text!) {
+            setTimer()
+        }
+        else {
+            //violate
+        }
     }
     
     
